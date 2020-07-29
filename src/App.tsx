38abee068a2fefe0b3867afced621c24d408597b
MartinPodhora <mato.podhora@gmail.com>
2020-07-29 15:56:00 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -6,70 +6,62 @@ import Button from "@material-ui/core/Button"
 import axios from "axios"
 import Task from "./components/task"
 
-interface SomeData {
-  data: string
-}
+// const renderList = (list : Array<typeof Task> | null) => {
+//   if(list !== null) {
+//     return list.map(task => <Task text={task.text} done={Task.done}/>)
+//   }
 
-const renderList = (list : Array<any> | null) => {
-  if(list !== null) {
-    return list.map(data => <Task text={data.text} done={data.done}/>)
-  }
-
-  return null
-}
+//   return null
+// }
 
 export const App: React.FC = () => {
   const [data, setData] = useState<{text: string}>({text: ""});
-  const [openSpan, setOpenSpan] = useState<boolean>(false); 
-  const [taskList, settaskList] = useState(null)
+  //const [openSpan, setOpenSpan] = useState<boolean>(false); 
+  const [taskList, settaskList] = useState(Array<typeof Task>())
 
-  if (taskList === null) {
+  if (taskList.length === 0) {
     axios.get('http://my-json-server.typicode.com/MartinPodhora/ToDoApp/db')
       .then(res => {
-          settaskList(res.data)   
+          settaskList(res.data.map(task => <Task text={task.text} done={task.done}/>))   
       })
+  }  
+
+  const handleChange = (event : any) => {
+    // save text of task
   }
-  
-    
 
-  // const handleChange = (event : any) => {
-  //   setOpenSpan(false);
-  //   const {name, value} = event.target;   //destruction
-  //   setData({...data, [name]: value})
-  // }
-
-  // const handleAdd = () => {
-  //   data ? setOpenSpan(true) : setOpenSpan(false)
-  // }
+  const handleAdd = () => {
+    // add task
+  }
 
   return (
     <div className="App">
-      <br /> {/*
-      <TextField 
-        name="Task"
-        id="ToDoItem" 
-        label="To Do Task Text" 
-        variant="outlined"
-        multiline={true}
-        className="toDoText"
-        style=  {{   
-                width: "500px",
-                }}
-        onChange={handleChange}
-      />
-      <Button 
-        variant="contained"
-        color="primary"
-        disableElevation
-        onClick={handleAdd}>
-          Add
-      </Button>
-              */}
+      <br />
+        <TextField 
+          name="Task"
+          id="ToDoItem" 
+          label="To Do Task Text" 
+          variant="outlined"
+          multiline={true}
+          className="toDoText"
+          style=  {{   
+                  width: "500px",
+                  }}
+          onChange={handleChange}
+        />
+        <Button 
+          variant="contained"
+          color="primary"
+          disableElevation
+          onClick={handleAdd}>
+            Add
+        </Button>
       <br />
       { /*openSpan ? <span>{data.text}</span> : ""*/}
       <table>
         {
-          renderList(taskList)
+          taskList?.map(data => data)
+          //renderList(taskList)
         }
       </table>
     </div>
